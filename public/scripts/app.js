377dc1e36a9e86128fbb59d3d68364f90f7dedc5
Andrew Sunada <andrewsunada@Andrews-MacBook-Pro.local>
2017-04-11 20:17:42 -0700

    Setting up board

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,34 @@
 (function(){
   'use strict';
   var app = angular.module('BattleshipApp', []);
-  app.controller('MainCtrl', [function(){
+  app.controller('MainCtrl', ['Board', function(Board){
     this.Message = "Battleship";
-    
+    this.Board = new Board();
+    console.log(this.Board);
   }]);
-
+  app.factory('Board', ['Piece', function(Piece){
+    var Board = function(){
+      this.Spaces = [];
+      this.Init = function(){
+        var row;
+        for(var i = 0; i < Rows; i++){
+          row = [];
+          for(var k = 0; k < Columns; k++){
+            row.push(new Piece(i,k));
+          }
+          this.Spaces.push(row);
+        }
+      };
+      this.Init();
+    },
+    Rows = 10, Columns = 10;
+    return Board;
+  }]);
+  app.factory('Piece', [function(){
+    var Piece = function(x, y){
+      this.X = x;
+      this.Y = y;
+    };
+    return Piece;
+  }]);
 })();
