4aabc04e44f7f50281dde00ed1acd9fadc6fa85e
Quinn Zepeda <developerquinnz@gmail.com>
2017-04-11 20:12:27 -0700

    more logic still

--- battleship.js
+++ battleship.js
@@ -6,4 +6,22 @@ function Ship(length) {
 	for(var i =0; i < length; i++) {
 		this.hits[i] = false;
 	}
+	this.isHit = function(x,y) {
+		for(var i = 0; i < length; i++) {
+			if(this.isHorizontal) {
+				var lx = this.x + i;
+				var ly = this.y
+			}
+			else {
+				var ly = this.y + i;
+				var lx = this.x
+			}
+			if(this.x === lx && this.y === ly) {
+				this.hits[i] = true;
+				return true;
+			}
+
+		}
+		return false;
+	}
 }
